77e3aafa2f809ad6e4f8b0048a500e6c7139b19d
Dariusz Niemczyk <palid@o2.pl>
2016-07-08 22:35:16 +0200

    Finally some real work on data.

--- Gambler.lua
+++ Gambler.lua
@@ -1,3 +1,70 @@
+-- WoW pseudomocks
+
+-- function CreateFrame()
+--   local frame = {}
+--   function frame:RegisterEvent()
+--   end
+--   function frame:UnregisterEvent()
+--   end
+--   function frame:SetScript()
+--   end
+--   function frame:AddMessage(message)
+--     print(message)
+--   end
+--   return frame
+-- end
+
+-- local DEFAULT_CHAT_FRAME = CreateFrame()
+
+-- local SLASH_Gambler1
+-- local SLASH_Gambler2
+-- local SlashCmdList = {}
+
+-- END OF MOCK WOW
+
+-- Very global globals, init that should be done ASAP.
+local GAMBLER_FRAME = CreateFrame('FRAME', 'GamblerFrame');
+
+-- GLOBALS
+local ADDON_NAME = 'Gambler'
+local SLASH_COMMAND_1 = '/gt'
+local SLASH_COMMAND_2 = '/gambler'
+local JOIN_GAME_CHARACTER = 'j'
+local LEAVE_GAME_CHARACTER = 'l'
+local DEFAULT_EVENT_TYPE = 'PARTY'
+-- TODO: Implement DEFAULT_EVENT_TYPE
+local EVENTS_NAMES = {
+  -- GUILD = {
+    -- 'CHAT_MSG_GUILD',
+    -- 'CHAT_MSG_GUILD_LEADER',
+  -- },
+  -- PARTY = {
+    'CHAT_MSG_PARTY',
+    'CHAT_MSG_PARTY_LEADER',
+    'CHAT_MSG_SYSTEM'
+  -- },
+  -- RAID = {
+    -- 'CHAT_MSG_RAID',
+    -- 'CHAT_MSG_RAID_LEADER',
+  -- }
+}
+local ROLL_STATE_NIL = 0
+local ROLL_STATE_JOINING = 1
+local ROLL_STATE_START = 2
+local ROLL_STATE_FINISHED = 3
+
+-- GAME STATE
+local GAME_STATE = {
+  -- uses above ROLL_STATE variables
+  rollState = 0,
+  currentDice = 100,
+  currentPlayers = {},
+  currentRolls = {
+    highest = 0,
+    lowest = 0
+  }
+}
+
 -- Utility functions
 
 -- Print contents of `tbl`, with indentation.
@@ -5,8 +72,8 @@
 local function tprint (tbl, indent)
   if not indent then indent = 0 end
   for k, v in pairs(tbl) do
-    formatting = string.rep("  ", indent) .. k .. ": "
-    if type(v) == "table" then
+    formatting = string.rep('  ', indent) .. k .. ': '
+    if type(v) == 'table' then
       print(formatting)
       tprint(v, indent+1)
     elseif type(v) == 'boolean' then
@@ -19,7 +86,6 @@ end
 
 local function size(keyset)
   local n=0
-
   for k,v in pairs(keyset) do
     n=n+1
   end
@@ -27,26 +93,7 @@ local function size(keyset)
 end
 
 local function capitalize(str)
-    return (str:gsub("^%l", string.upper))
-end
-
-local function split_string(str, separator)
-  if separator == nil then
-    return {str}
-  end
-  local list = {}
-  local temporary_str = ''
-  for i = 1, string.len(str) do
-    local char = string.sub(str, i, i)
-    if char == separator then
-      table.insert(list, temporary_str)
-      temporary_str = ''
-    else
-      temporary_str = temporary_str .. char
-    end
-  end
-  table.insert(list, temporary_str)
-  return list
+    return (str:gsub('^%l', string.upper))
 end
 
 local function logMessage(message)
@@ -76,10 +123,98 @@ local function hexrgb(hexred, hexgreen, hexblue, text)
   return '|c' .. hexred .. hexgreen .. hexblue .. text
 end
 
--- Global variables
-local addon_name = "Gambler"
-local SLASH_COMMAND_1 = '/gt'
-local SLASH_COMMAND_2 = '/gambler'
+local function sendToChat(message)
+    SendChatMessage(message, 'RAID', nil)
+end
+
+local function normalizeCharacterName(nickname)
+  local lower = strlower(nickname)
+  local nickWithoutServer, server = strsplit('-', lower)
+  return nickWithoutServer
+end
+
+
+-- INITIALIZATION --
+-- Create global context managed by WoW.
+if (not Gambler) then
+  Gambler = {
+    alts = {},
+    bans = {},
+    stats = {},
+    config = {
+      autoCloseIn = 60  -- seconds
+    }
+  }
+end
+
+local function parseRoll(message)
+  local player, systemCommand, roll, range = strsplit(' ', message)
+  local normalizedCharacterName = normalizeCharacterName(player)
+  if (systemCommand == 'rolls' and GAME_STATE.currentPlayers[player]) then
+    local minRoll, maxRoll = strsplit('-', range)
+    local parsedRoll = tonumber(roll)
+    -- TODO: WTF is minRoll - taken out of CrossGambling.
+    minRoll = tonumber(strsub(minRoll, 2))
+    maxRoll = tonumber(strsub(maxRoll, 1, -2))
+    if (maxRoll == currentDice and minRoll == 1) then
+      -- TODO - tiebreaking, actual counting, stats, whatever
+  end
+end
+
+local function onChatMessage(...)
+  if GAME_STATE.rollState == ROLL_STATE_JOINING then
+    local arguments = {...}
+    local eventName = arguments[2]
+    local message = arguments[3]
+    local characterName = normalizeCharacterName(arguments[4])
+    -- TODO: implement DEFAULT_EVENT_TYPE
+    if eventName == 'CHAT_MSG_RAID' or 'CHAT_MSG_RAID_LEADER' or 'CHAT_MSG_PARTY' or 'CHAT_MSG_PARTY_LEADER' then
+      if message == JOIN_GAME_CHARACTER then
+        -- TODO: implement alts
+        if Gambler.bans[characterName] ~= nil then
+          sendToChat(string.format('%s is banned from Gambler! No games for you!', characterName))
+          return -- just do a clean exit if character is banned.
+        end
+        GAME_STATE.currentPlayers[characterName] = true
+      elseif message == LEAVE_GAME_CHARACTER then
+        if GAME_STATE.currentPlayers[characterName] == true then
+          GAME_STATE.currentPlayers[characterName] = nil
+        end
+      end
+    elseif eventName == 'CHAT_MSG_SYSTEM' then
+      return parseRoll(message)
+    end
+  else
+  end
+end
+
+local function onSlashCallback(message)
+  local splitted = strsplit(' ', message)
+  local command = splitted[1]
+  if (command == nil) then
+    command = ''
+  else
+    command = strlower(command)
+  end
+  local commandFunction = AvailableCommands[command]
+  if commandFunction ~= nil then
+    print(select(1, unpack(splitted)))
+    commandFunction.fnc(select(1, unpack(splitted)))
+  else
+    logMessage('Command ' .. command .. ' does not exist. Try ' .. SLASH_COMMAND_1 .. ' help for list of commands.')
+  end
+end
+
+logMessage('|cffffff00<Gambler 0.01> loaded /gt to use')
+
+GAMBLER_FRAME:SetScript('OnEvent', onChatMessage)
+SLASH_Gambler1 = SLASH_COMMAND_1
+SLASH_Gambler2 = SLASH_COMMAND_2
+SlashCmdList[ADDON_NAME] = onSlashCallback
+
+
+-- Commands
+
 local AvailableCommands = {
   help = {
     helptext = 'lists all available commands.'
@@ -91,10 +226,19 @@ local AvailableCommands = {
     helptext = 'shows banned players list.'
   },
   ban = {
-    helptext = 'add player to banlist. Requires arguments [player] [reason]'
+    helptext = 'add player to banlist. Requires arguments [player] [reason].'
   },
   unban = {
     helptext = 'remove player from banlist.'
+  },
+  roll = {
+    helptext = 'starts the casino with [sided] dice.'
+  },
+  throw = {
+    helptext = string.format('close the current casino and start throwing the dices. Will automatically close after [%s] seconds.', Gambler.config.autoCloseIn)
+  },
+  reset = {
+    helptext = 'clean up current game and close the casino. That is all folks, no more games now.'
   }
 }
 function AvailableCommands.stats:fnc(player)
@@ -102,8 +246,8 @@ function AvailableCommands.stats:fnc(player)
 end
 function AvailableCommands.help:fnc()
   logMessage('List of all available commands:')
-  for k, v in pairs(AvailableCommands) do
-    logMessage(capitalize(k) .. ': ' .. v.helptext .. '.')
+  for command, value in pairs(AvailableCommands) do
+    logMessage(string.format('%s:%s.', capitalize(command), value.helptext))
   end
 end
 function AvailableCommands.bans:fnc()
@@ -112,7 +256,7 @@ function AvailableCommands.bans:fnc()
   else
     logMessage('Banned players list with reasons :')
     for user, reason in pairs(Gambler.bans) do
-      logMessage(user .. ': banned for ' .. reason)
+      logMessage(string.format('%s: banned for %s', user, reason))
     end
   end
 end
@@ -124,10 +268,10 @@ function AvailableCommands.ban:fnc(player, ...)
     if reason ~= nil then
       local bannedPlayer = Gambler.bans[capitalizedPlayer];
       if bannedPlayer ~= nil then
-        logMessage(capitalizedPlayer .. ' was already banned for reason: ' .. reason)
+        logMessage(string.format('%s was already banned for reason: %s', capitalizedPlayer, reason))
       else
         Gambler.bans[capitalizedPlayer] = reason
-        logMessage(capitalizedPlayer .. ' was banned for a reason: ' .. reason)
+        logMessage(string.format('%s was banned for a reason: %s', capitalizedPlayer, reason))
       end
     else
       logMessage('Reason is required to ban a user.')
@@ -148,66 +292,41 @@ function AvailableCommands.unban:fnc(player)
     logMessage('/unban palid')
   end
 end
-
--- INITIALIZATION --
-
--- Create global context managed by WoW.
-
-if (not Gambler) then
-  Gambler = {
-    alts = {},
-    bans = {},
-    stats = {}
-  }
-end
-
-local function onChatMessage(...)
-  local arguments = {...}
-  local EVENT_NAME = arguments[2]
-  local MESSAGE = arguments[3]
-  local SENDER_NAME = arguments[4]
-end
-
--- LOAD FUNCTION --
-local function init()
-  local frame = CreateFrame("FRAME", "GamblerFrame");
-  logMessage("|cffffff00<Gambler 0.01> loaded /gt to use")
-
-
-  local message_event_sources = {'PARTY', 'RAID', 'GUILD'}
-
-  for index, event_name in pairs(message_event_sources) do
-    local event_name = "CHAT_MSG_" .. event_name
-    frame:RegisterEvent(event_name)
-    frame:RegisterEvent(event_name .. '_LEADER')
-  end
-  -- frame:RegisterEvent("CHAT_MSG_SYSTEM")
-  -- frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-  frame:SetScript("OnEvent", onChatMessage)
-end
-
-local function onSlashCallback(message)
-  local splitted = split_string(message, ' ')
-  local command = splitted[1]
-  if (command == nil) then
-    command = ''
+function AvailableCommands.roll:fnc(dicesSides)
+  if GAME_STATE.rollState == ROLL_STATE_RESET then
+    for index, eventName in pairs(EVENTS_NAMES) do
+      GAMBLER_FRAME:RegisterEvent(eventName)
+    end
+    local dicesSidesNumber = tonumber(dicesSides);
+    if dicesSides == nil then dicesSides = 100 end
+    GAME_STATE.rollState = ROLL_STATE_JOINING
+    GAME_STATE.currentPlayers = {}
+    GAME_STATE.currentDice = dicesSides
+    sendToChat(string.format('Gambler set up casino rolls with [%s] sided dice!', dicesSides))
+    sendToChat(string.format('Type [%s] to join the game or [%s] to leave it.'), JOIN_GAME_CHARACTER, LEAVE_GAME_CHARACTER)
+    C_Timer.After(Gambler.config.autoCloseIn, AvailableCommands.throw.fnc)
   else
-    command = command:lower()
+    sendToChat(string.format("Gambler still has it's game with [%s] sided dice open!", GAME_STATE.currentDice))
   end
-  local commandFunction = AvailableCommands[command]
-  if commandFunction ~= nil then
-    print(select(1, unpack(splitted)))
-    commandFunction.fnc(select(1, unpack(splitted)))
-  else
-    logMessage('Command ' .. command .. ' does not exist. Try ' .. SLASH_COMMAND_1 .. ' help for list of commands.')
-  end
-  -- logMessage(splitted[0])
-  -- logMessage(message or 'nil')
 end
+function AvailableCommands.throw:fnc()
+  if GAME_STATE.rollState == ROLL_STATE_JOINING then
+    if size(GAME_STATE.currentPlayers) > 2 then
+      sendToChat('Gambler is finished waiting! Time to roll your [%s] sided dices!', GAME_STATE.currentDice)
+      GAME_STATE.rollState = ROLL_STATE_START
+    end
+  end
+end
+function AvailableCommands.reset:fnc()
+  GAME_STATE.rollState = ROLL_STATE_RESET
+  GAME_STATE.currentPlayers = {}
+  GAME_STATE.currentDice = 100
+  GAME_STATE.currentRolls.highest = 0
+  GAME_STATE.currentRolls.lowest = 0
+  sendToChat('Gambler has hidden his casino for now.')
+  for index, eventName in pairs(EVENTS_NAMES) do
+    GAMBLER_FRAME.UnregisterEvent(eventName)
+  end
 
-SLASH_Gambler1 = SLASH_COMMAND_1
-SLASH_Gambler2 = SLASH_COMMAND_2
-SlashCmdList[addon_name] = onSlashCallback
-
-
-init()
+  -- GAMBLER_FRAME.UnregisterEvent('OnEvent')
+end
